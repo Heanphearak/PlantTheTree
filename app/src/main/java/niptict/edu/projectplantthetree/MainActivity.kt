17353ca142608cb6f567d8a2ce1bfbdbfc3b76c1
Heanphearak <phearakhean135@gmail.com>
2019-12-25 16:40:55 +0700

    navigation

--- app/src/main/java/niptict/edu/projectplantthetree/MainActivity.kt
+++ app/src/main/java/niptict/edu/projectplantthetree/MainActivity.kt
@@ -2,11 +2,16 @@ package niptict.edu.projectplantthetree
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = findNavController(R.id.myNavHostFragment)
+        bottom_navigation.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/niptict/edu/projectplantthetree/MyTreeFragment.kt
@@ -0,0 +1,20 @@
+package niptict.edu.projectplantthetree
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MyTreeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_tree, container, false)
+    }
+
+}
