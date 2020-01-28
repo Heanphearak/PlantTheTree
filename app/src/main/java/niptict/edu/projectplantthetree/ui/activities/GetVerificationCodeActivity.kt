abdd38dfc442b26bd0690f271e28b78e2ac98552
unknown <phearakhean135@gmail.com>
2020-01-28 11:29:22 +0700

    add verify

---
+++ app/src/main/java/niptict/edu/projectplantthetree/ui/activities/GetVerificationCodeActivity.kt
@@ -0,0 +1,12 @@
+package niptict.edu.projectplantthetree
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GetVerificationCodeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_verification_code)
+    }
+}
